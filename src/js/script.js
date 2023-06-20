858715a4030725dc85ac68236588f57b7a12a590
Kacper Kosicki <kosickikacper1@gmail.com>
2023-06-20 20:12:30 +0200

    Filtering books using a form

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,36 @@
 'use strict';
 
 const favoriteBooks = [];
+const filters = [];
 
 function initActions() {
-  const bookImages = document.querySelectorAll('.book__image');
+  const bookList = document.querySelector('.books-list');
+  const form = document.querySelector('.filters');
 
-  bookImages.forEach(bookImage => {
-    bookImage.addEventListener('dblclick', function(event) {
+  form.addEventListener('click', function(event) {
+    const target = event.target;
+
+    if (target.tagName === 'INPUT' && target.type === 'checkbox' && target.name === 'filter') {
+      const filterValue = target.value;
+
+      if (target.checked) {
+        filters.push(filterValue);
+      } else {
+        const index = filters.indexOf(filterValue);
+        filters.splice(index, 1);
+      }
+
+      filterBooks();
+    }
+  });
+
+  bookList.addEventListener('dblclick', function(event) {
+    const target = event.target;
+
+    if (target.classList.contains('book__image') || target.offsetParent.classList.contains('book__image')) {
       event.preventDefault();
+
+      const bookImage = target.closest('.book__image');
       const bookId = bookImage.dataset.id;
       const isFavorite = favoriteBooks.includes(bookId);
 
@@ -19,7 +42,7 @@ function initActions() {
         favoriteBooks.push(bookId);
         bookImage.classList.add('favorite');
       }
-    });
+    }
   });
 }
 
@@ -31,6 +54,30 @@ function render() {
     const generatedHTML = Handlebars.compile(template)(book);
     const bookElement = utils.createDOMFromHTML(generatedHTML);
     booksList.appendChild(bookElement);
+  });
+}
+
+function filterBooks() {
+  const bookImages = document.querySelectorAll('.book__image');
+
+  bookImages.forEach(bookImage => {
+    const bookId = bookImage.dataset.id;
+    const book = dataSource.books.find(book => book.id == bookId);
+
+    let shouldBeHidden = false;
+
+    for (const filter of filters) {
+      if (!book.details[filter]) {
+        shouldBeHidden = true;
+        break;
+      }
+    }
+
+    if (shouldBeHidden) {
+      bookImage.classList.add('hidden');
+    } else {
+      bookImage.classList.remove('hidden');
+    }
   });
 }
 
