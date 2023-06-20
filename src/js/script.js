ee8d9b6332dbb6c0e2a4a5a45dbb4ea62519eb86
Kacper Kosicki <kosickikacper1@gmail.com>
2023-06-20 23:28:32 +0200

    Blad z ...

--- src/js/script.js
+++ src/js/script.js
@@ -1,101 +1,113 @@
 'use strict';
 
-const favoriteBooks = [];
-const filters = [];
+class BooksList {
+  constructor() {
+    this.favoriteBooks = [];
+    this.filters = [];
+    this.initData();
+    this.getElements();
+    this.render();
+    this.initActions();
+  }
 
-function initActions() {
-  const bookList = document.querySelector('.books-list');
-  const form = document.querySelector('.filters');
+  initData() {
+    this.data = dataSource.books;
+  }
 
-  form.addEventListener('click', function(event) {
-    const target = event.target;
+  getElements() {
+    this.bookList = document.querySelector('.books-list');
+    this.form = document.querySelector('.filters');
+  }
 
-    if (target.tagName === 'INPUT' && target.type === 'checkbox' && target.name === 'filter') {
-      const filterValue = target.value;
+  initActions() {
+    this.form.addEventListener('click', (event) => {
+      const target = event.target;
 
-      if (target.checked) {
-        filters.push(filterValue);
-      } else {
-        const index = filters.indexOf(filterValue);
-        filters.splice(index, 1);
+      if (target.tagName === 'INPUT' && target.type === 'checkbox' && target.name === 'filter') {
+        const filterValue = target.value;
+
+        if (target.checked) {
+          this.filters.push(filterValue);
+        } else {
+          const index = this.filters.indexOf(filterValue);
+          this.filters.splice(index, 1);
+        }
+
+        this.filterBooks();
       }
+    });
 
-      filterBooks();
-    }
-  });
+    this.bookList.addEventListener('dblclick', (event) => {
+      const target = event.target;
 
-  bookList.addEventListener('dblclick', function(event) {
-    const target = event.target;
+      if (target.classList.contains('book__image') || target.offsetParent.classList.contains('book__image')) {
+        event.preventDefault();
 
-    if (target.classList.contains('book__image') || target.offsetParent.classList.contains('book__image')) {
-      event.preventDefault();
+        const bookImage = target.closest('.book__image');
+        const bookId = bookImage.dataset.id;
+        const isFavorite = this.favoriteBooks.includes(bookId);
 
-      const bookImage = target.closest('.book__image');
+        if (isFavorite) {
+          const index = this.favoriteBooks.indexOf(bookId);
+          this.favoriteBooks.splice(index, 1);
+          bookImage.classList.remove('favorite');
+        } else {
+          this.favoriteBooks.push(bookId);
+          bookImage.classList.add('favorite');
+        }
+      }
+    });
+  }
+
+  render() {
+    const template = document.getElementById('template-book').innerHTML;
+
+    this.data.forEach((book) => {
+      const ratingBgc = this.determineRatingBgc(book.rating);
+      const ratingWidth = book.rating * 10;
+
+      const generatedHTML = Handlebars.compile(template)({ book, ratingBgc, ratingWidth });
+      const bookElement = utils.createDOMFromHTML(generatedHTML);
+      this.bookList.appendChild(bookElement);
+    });
+  }
+
+  filterBooks() {
+    const bookImages = document.querySelectorAll('.book__image');
+
+    bookImages.forEach((bookImage) => {
       const bookId = bookImage.dataset.id;
-      const isFavorite = favoriteBooks.includes(bookId);
+      const book = this.data.find((book) => book.id == bookId);
 
-      if (isFavorite) {
-        const index = favoriteBooks.indexOf(bookId);
-        favoriteBooks.splice(index, 1);
-        bookImage.classList.remove('favorite');
+      let shouldBeHidden = false;
+
+      for (const filter of this.filters) {
+        if (!book.details[filter]) {
+          shouldBeHidden = true;
+          break;
+        }
+      }
+
+      if (shouldBeHidden) {
+        bookImage.classList.add('hidden');
       } else {
-        favoriteBooks.push(bookId);
-        bookImage.classList.add('favorite');
+        bookImage.classList.remove('hidden');
       }
-    }
-  });
-}
+    });
+  }
 
-function render() {
-  const template = document.getElementById('template-book').innerHTML;
-  const booksList = document.querySelector('.books-list');
-
-  dataSource.books.forEach(book => {
-    const ratingBgc = determineRatingBgc(book.rating);
-    const ratingWidth = book.rating * 10;
-  
-    const generatedHTML = Handlebars.compile(template)({ ...book, ratingBgc, ratingWidth });
-    const bookElement = utils.createDOMFromHTML(generatedHTML);
-    booksList.appendChild(bookElement);
-  });
-
-}
-
-function filterBooks() {
-  const bookImages = document.querySelectorAll('.book__image');
-
-  bookImages.forEach(bookImage => {
-    const bookId = bookImage.dataset.id;
-    const book = dataSource.books.find(book => book.id == bookId);
-
-    let shouldBeHidden = false;
-
-    for (const filter of filters) {
-      if (!book.details[filter]) {
-        shouldBeHidden = true;
-        break;
-      }
-    }
-
-    if (shouldBeHidden) {
-      bookImage.classList.add('hidden');
+  determineRatingBgc(rating) {
+    if (rating < 6) {
+      return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
     } else {
-      bookImage.classList.remove('hidden');
+      return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
     }
-  });
-}
-
-function determineRatingBgc(rating) {
-  if (rating < 6) {
-    return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
-  } else if (rating > 6 && rating <= 8) {
-    return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
-  } else if (rating > 8 && rating <= 9) {
-    return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
-  } else {
-    return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
   }
 }
 
-render();
-initActions();
+const app = new BooksList();
+app.bookList();
